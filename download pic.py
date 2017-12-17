784cc8674348229751bcff558641bf7d418d6bcf
Chief-P <30769555+Chief-P@users.noreply.github.com>
2017-12-17 15:28:13 +0800

    fetch picture

---
+++ download pic.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 17 14:50:45 2017
+
+@author: propertime
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import json
+"""
+def fetch():
+    url = 
+    headers = {Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36}
+    resp = requests.get(url, headers = headers)
+    j = json.loads(resp.text)
+    return resp
+
+def parse(resp):
+    soup = BeautifulSoup.find_all(')
+"""
+def fetch():
+    url = "https://www.wikipedia.org/portal/wikipedia.org/assets/img/Wikipedia-logo-v2.png"
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'}
+    resp = requests.get(url, headers = headers)
+    return resp
+    
+if __name__ == '__main__':
+    responce = fetch()
+    with open("F://cs inf/wiki_logo.jpg", "wb") as fp:
+        fp.write(responce.content)
+    
